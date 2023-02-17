8388ea3c92940287a4b826c333c78c6d0ada1025
Angel Gascon <angel.gascon@estudiants.urv.cat>
2023-02-17 13:11:58 +0100

    commentaris i docu LAB1 fixed

--- LAB1/actualitzat.sh
+++ LAB1/actualitzat.sh
@@ -11,7 +11,7 @@ fi
 
 # Iterate through the arguments provided
 for package in "$@"; do
-  # Check if the package is installed, otherwise check if package has upgrades
+  # Check if the package is not installed, otherwise check if package has upgrades
   if ! apt list --installed 2>/dev/null | grep -q "^$package/"; then
     # not installed case
     echo "$(date +'%Y-%m-%d %H:%M:%S'), Package $package is not installed."
@@ -21,7 +21,7 @@ for package in "$@"; do
   else
     # has upgrades case
     new_version=$(apt list --upgradable 2>/dev/null | grep "\b$package/" | cut -d' ' -f2 | cut -d'/' -f1)
-    echo "$(date '+%Y-%m-%d %H:%M:%S'), $package actualitzable a la versió $new_version" | logger
+    echo "$(date '+%Y-%m-%d %H:%M:%S'), $package upgradeable to version $new_version" | logger
   fi
   fi
 done
